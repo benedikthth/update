0f44126a5e57899bbca48ecf7d4f796181fd07dc
benedikt <benedikthth@gmail.com>
2017-03-26 20:51:53 +0000

    some revision

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-;
+import image from './profile.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={image} className="App-profile" alt="logo" />
           <h2>Welcome to Spock.is</h2>
           <h3>This is Work in progress</h3>
+
         </div>
       </div>
     );
