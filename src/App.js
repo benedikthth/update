1b17892c84233ec0ede1c030559f3489677d8786
benedikt <benedikthth@gmail.com>
2017-03-27 13:07:28 +0000

    added tabs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import image from './profile.jpg';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
+
+Tabs.setUseDefaultStyles(false);
 class App extends Component {
   render() {
     return (
@@ -10,6 +13,23 @@ class App extends Component {
           <img src={image} className="App-profile" alt="logo" />
           <h2>Welcome to Spock.is</h2>
           <h3>This is Work in progress</h3>
+
+
+          <Tabs onSelect={this.handleSelect} selectedIndex={0}>
+            <TabList>
+              <Tab>Introduction</Tab>
+              <Tab>Contact</Tab>
+            </TabList>
+
+            <TabPanel>
+              <h2>Hello from Foo</h2>
+            </TabPanel>
+            <TabPanel>
+              <h2>Hello from Bar</h2>
+            </TabPanel>
+          </Tabs>
+
+
 
         </div>
       </div>
